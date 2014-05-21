147c59e65be35218f276b93c87c07ca63a880ab2
Knut Hühne <knut@k-nut.eu>
2014-05-21 10:27:03 +0200

    Added bootstrap, two more graphs

--- freakshow.js
+++ freakshow.js
@@ -1,33 +1,15 @@
+/*global $ */
+
 function timeSort(a, b){
   return a[1] - b[1];
 }
 
-var activity = [];
-var loudnesss_shortterm = [];
+function inverseSort(a, b){
+  return -1* (a[1] - b[1]);
+}
 
-$.getJSON( "./fs132-denk-nicht-in-layern-denk-in-schichten.json", function( data ) {
-  var items = [];
-  console.log(data);
-  $.each( data.statistics.tracks, function( key, val ) {
-    var total = 0;
-    $.each(val.activity, function(key, val){
-      total += val[1] - val[0];
-    });
-    activity.push([val.identifier, Math.round(total/60)]);
-    loudnesss_shortterm.push([val.identifier, val.levels.max_shortterm[0]]);
-    console.log(val.identifier + ': ' + Math.round(total/60) + ' min');
-    console.log(val.identifier + ': momentary:  ' + val.levels.max_momentary[0]);
-    console.log(val.identifier + ': short term:  ' + val.levels.max_shortterm[0]);
-
-  });
-  activity.sort(timeSort);
-}).done(function() {plotData(activity);});
-
-function plotData(activity){
-
-  var data = [ ["January", 10], ["February", 8], ["March", 4], ["April", 13], ["May", 17], ["June", 9] ];
-
-  $.plot("#placeholder", [ activity ], {
+function plotData(activity, divId){
+  $.plot(divId, [ activity ], {
     series: {
       bars: {
         show: true,
@@ -41,4 +23,32 @@ function plotData(activity){
     }
   });
 }
+
+var activity = [];
+var loudness_shortterm = [];
+var loudness_momentary = [];
+
+$.getJSON( "./fs132-denk-nicht-in-layern-denk-in-schichten.json", function( data ) {
+  var items = [];
+  console.log(data);
+  $.each( data.statistics.tracks, function( key, val ) {
+    var total = 0;
+    $.each(val.activity, function(key, val){
+      total += val[1] - val[0];
+    });
+    activity.push([val.identifier, Math.round(total/60)]);
+    loudness_shortterm.push([val.identifier, val.levels.max_shortterm[0]]);
+    loudness_momentary.push([val.identifier, val.levels.max_momentary[0]]);
+    console.log(val.identifier + ': ' + Math.round(total/60) + ' min');
+    console.log(val.identifier + ': momentary:  ' + val.levels.max_momentary[0]);
+    console.log(val.identifier + ': short term:  ' + val.levels.max_shortterm[0]);
+
+  });
+  activity.sort(timeSort);
+}).done(function() {
+  plotData(activity.sort(timeSort), "#placeholder");
+  plotData(loudness_shortterm.sort(inverseSort), "#loudnessShortTerm");
+  plotData(loudness_momentary.sort(inverseSort), "#loudnessMomentary");
+});
+
 
